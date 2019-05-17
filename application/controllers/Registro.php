1b25ec8f765bd3d829d38cd5483f9804be1edd86
Brando Jesus <brandojesus011@gmail.com>
2019-05-17 17:11:06 -0500

    cambio en config, database

--- application/controllers/Registro.php
+++ application/controllers/Registro.php
@@ -4,11 +4,23 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Registro extends CI_Controller {
     public function __construct() {
         parent::__construct();
-        $this->load->helper(array('getmenu','url'));
+        $this->load->helper(array('getmenu'));
+        $this->load->database();
     }
     
     public function index() {
         $data['menu'] = main_menu();
         $this->load->view('registro', $data);
+        $query = $this->db->get('Usuarios');
+        var_dump($query->result());
+
+    }
+
+    public function create() {
+        $username = $this->input->post('username');
+        $email = $this->input->post('email');
+        $password = $this->input->post('password');
+        $password_c = $this->input->post('password_confirm');
+        var_dump($username . $email . $password . $password_c);
     }
 }
--- application/views/registro.php
+++ application/views/registro.php
@@ -13,5 +13,28 @@
             <li><a href="<?=$item['url']?>"><?=$item['title']?></a></li>
         <?php endforeach; ?>
     </ul>
+
+    <?php
+        echo form_open('registro/create', array('method' => 'POST'));
+        echo form_label('Nombre de usuario: ');
+        echo form_input('username');
+        
+        echo '<br>';
+        echo form_label('Correo Electrónico: ');
+        echo form_input(array('type'=> 'email', 'name' => 'email'));
+
+        echo '<br>';
+        echo form_label('Contraseña: ');
+        echo form_password('password');
+
+        echo '<br>';
+        echo form_label('Confirmación de contraseña: ');
+        echo form_password('password_confirm');
+
+        echo '<br>';
+        echo form_submit('submit','Enviar Datos');
+
+        echo form_close();
+    ?>
 </body>
 </html>
